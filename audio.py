d989ae3bf8cf6dead31a989d322816ea8d5290ee
Xie Renhao <renhao.xie@intel.com>
2022-01-21 14:23:36 +0800

    add comments

--- audio.py
+++ audio.py
@@ -16,22 +16,59 @@ class Audio:
         self.__analyze_audio()
         pass
 
-    def is_audio_beat(self, motion_beat_frame):
+    def is_audio_beat(self, motion_beat_frame, slack_range=8) -> bool:
+        ''' Return whether there is a matched audio beat.
+        
+        Parameters:
+        ----------
+        motion_beat_frame : int 
+          - Frame index of an detected motion beat.
+
+        slack_range : int
+          - A frame range centered on motion_beat_frame in which we search for a matching audio beat.
+
+        Return:
+        ----------
+        True/False
+        '''
         assert isinstance(motion_beat_frame, int), 'Input parameter must be integer'
-        for i in range(motion_beat_frame-5, motion_beat_frame+5, 1):
+        for i in range(motion_beat_frame-(slack_range//2), motion_beat_frame+(slack_range//2), 1):
             if i in self.audio_beats: return True
         return False
 
 
-    def get_effect_advice(self, motion_beat_frame):
+    def get_effect_advice(self, motion_beat_frame, slack_range=8, farthest_onset=20) -> dict:
+        ''' Return effect advice dict.
+        
+        Parameters:
+        ----------
+        motion_beat_frame : int 
+          - Frame index of an detected motion beat.
+
+        slack_range : int
+          - A frame range centered on motion_beat_frame in which we search for a matching audio beat.
+        
+        farthest_onset : int
+          - If we can find an onset within [motion_beat_frame-farthest_onset, motion_beat_frame), 
+          it will be determined as the start frame. Otherwise the start frame will be the same as key frame.
+
+        Return:
+        ----------
+        res : dict
+          - {'is_audio_beat':False, 'start_frame':-1, 'key_frame':-1, 'speed':-1}
+        '''
         assert isinstance(motion_beat_frame, int), 'Input parameter must be integer'
-        res = {'is_audio_beat':False, 'start_frame':-1, 'key_frame':-1}
-        for frame_idx in range(motion_beat_frame-5, motion_beat_frame+5, 1):
+        assert isinstance(slack_range, int), 'Input parameter must be integer'
+        assert isinstance(farthest_onset, int), 'Input parameter must be integer'
+
+        res = {'is_audio_beat':False, 'start_frame':-1, 'key_frame':-1, 'speed':-1}
+        for frame_idx in range(motion_beat_frame-(slack_range//2), motion_beat_frame+(slack_range//2), 1):
             if frame_idx in self.audio_beats:
                 res['is_audio_beat'] = True
                 res['key_frame'] = frame_idx
                 res['start_frame'] = frame_idx
-                for i in range(frame_idx, frame_idx-30, -1):
+                res['speed'] = self.audio_beats[frame_idx]['speed']
+                for i in range(frame_idx, frame_idx-farthest_onset, -1):
                     if i in self.audio_onsets:
                         res['start_frame'] = i
         return res
