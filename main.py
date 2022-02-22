2a037fd1edca611fcfd8c0fa1163e783480e493a
YaruDu <yaru.du@intel.com>
2022-02-22 22:38:13 +0800

    Update motion beat analysis

--- main.py
+++ main.py
@@ -7,15 +7,14 @@ def main():
     m = Motion(video_path)
     audio_beats = a.audio_beats
     motion_beats = m.motion_beats
-
-    for keyframe_index, motion_effect_hint in motion_beats.items():
-            if  a.is_audio_beat(keyframe_index):
-                effect_advice = a.get_effect_advice(keyframe_index) 
-                endframe_index  = effect_advice["endframe_index"]
-                startframe_index = effect_advice["startframe_index"]            
-                do_effect( motion_effect_hint, startframe_index, keyframe_index, endframe_index)
+ 
+    for frame_idx, item in audio_beats.items():
+            if  m.is_motion_beat(frame_idx):
+                previous_frame_idx = -1
+                effect_advice = m.get_effect_advice(previous_frame_idx, frame_idx)            
+                #do_effect( motion_effect_hint, startframe_index, keyframe_index, endframe_index)
     
-    save_effected_video()
+    #save_effected_video()
 
 
 if __name__ == '__main__':
