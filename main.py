01bf8ac3903bba5dda9085af9930f1956b2a18c2
xrhintelacc <renhao.xie@intel.com>
2022-04-06 16:07:47 +0800

    refactor the code

--- main.py
+++ main.py
@@ -1,22 +1,53 @@
+import os
+import argparse
+
 from audio import Audio
+from visualization import draw_shapes_to_special_images, visualization
 from motion import Motion
+from effect_decision import EffectDecision
+from video_encoding import VideoEncoding
 
-def main():
-    video_path = 'resources_video/money_reference.mp4'
-    a = Audio(video_path)
-    m = Motion(video_path)
-    audio_beats = a.audio_beats
-    motion_beats = m.motion_beats
+def main(video_path, output_dir, debug):
+    # analyze motion
+    m = Motion(video_path=video_path, debug=debug)
+    area_data = m.area_data
 
-    for keyframe_index, motion_effect_hint in motion_beats.items():
-            if  a.is_audio_beat(keyframe_index):
-                effect_advice = a.get_effect_advice(keyframe_index) 
-                endframe_index  = effect_advice["endframe_index"]
-                startframe_index = effect_advice["startframe_index"]            
-                do_effect( motion_effect_hint, startframe_index, keyframe_index, endframe_index)
-    
-    save_effected_video()
+    # analyze audio
+    ado = Audio(video_path)
+    audio_beats = ado.audio_beats
+    framerate = ado.framerate
+    beats = []
+    for k, v in audio_beats.items():
+        beats.append(k)
+
+    # get effects based on music and motion
+    eff = EffectDecision(debug=debug)
+    res, start_list, key_list, end_list, zoom_scale_list, group_list = eff.analyze_motion(area_data, beats, ado, framerate, group_size=4)
+
+    # generate edited video
+    video_name = os.path.splitext(os.path.basename(video_path))[0]
+    out_path = os.path.join(output_dir, '{}_out.mp4'.format(video_name))
+    enc = VideoEncoding()
+    enc.gen_effects(res, video_in_path=video_path, video_out_path=out_path)
+
+    if debug is True:
+        visualization(area_data, start_list, key_list, end_list, video_name, beats, group_list, zoom_scale_list)
+        infer_debug_folder = os.path.join('detection_result', video_name)  # ToDo: use Infer.debug_folder
+        draw_shapes_to_special_images(infer_debug_folder, start_list, key_list, end_list, beats)
 
 
 if __name__ == '__main__':
-    main()
+    # ToDo: Use arg parser
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--video-path', type=str, default='resources_video/spring_origin.mp4', help='video path')
+    parser.add_argument('--output-dir', type=str, default='output', help='output dir')
+    parser.add_argument('--debug', default=False, help='debug')
+    args = parser.parse_args()
+
+    if not os.path.exists(args.video_path):
+        print('{} does not exit!'.format(args.video_path))
+
+    if not os.path.exists(args.output_dir):
+        os.mkdir(args.output_dir)
+
+    main(args.video_path, args.output_dir, args.debug)
---
+++ visualization.py
@@ -0,0 +1,108 @@
+import cv2
+import os
+
+
+def draw_text(img, text,
+              font=cv2.FONT_HERSHEY_PLAIN,
+              pos=(0, 0),
+              font_scale=3,
+              font_thickness=2,
+              text_color=(0, 255, 0),
+              text_color_bg=(0, 0, 0)
+              ):
+
+    x, y = pos
+    text_size, _ = cv2.getTextSize(text, font, font_scale, font_thickness)
+    text_w, text_h = text_size
+    cv2.rectangle(img, pos, (x + text_w, y + text_h), text_color_bg, -1)
+    cv2.putText(img, text, (x, y + text_h + font_scale - 1), font, font_scale, text_color, font_thickness)
+
+    return text_size
+
+
+def draw_circle_to_images(img_folder, img_idx_list, pos, radius, color):
+    import cv2
+    # draw shapes to the detection results
+    for img_idx in img_idx_list:
+        img_name='{}/{}.png'.format(img_folder, img_idx)
+        img = cv2.imread(img_name)
+        cv2.circle(img, pos, radius, color, -1)
+        cv2.imwrite(img_name, img)
+
+
+def draw_shapes_to_special_images(img_folder, start_list, key_list, end_list, beats, strong_beats=None):
+    exist_and_not_empty = os.listdir(img_folder)
+    if exist_and_not_empty is False:
+        print('{} not exist or empty'.format(img_folder))
+        return
+
+    first_img = cv2.imread('{}/1.png'.format(img_folder))
+    height = first_img.shape[0]
+    width = first_img.shape[1]
+    radius = int(height/8)
+    pos_right_top1 = (width-radius, radius)
+    pos_right_top2 = (width-radius, radius*3)
+    pos_right_top3 = (width-radius, radius*5)
+    pos_right_top4 = (width-radius, radius*7)
+
+    pos_left_bottom = (radius, radius*7)
+
+    # BGR values
+    black = (0, 0, 0)
+    red = (0, 0, 255)
+    green = (0, 255, 0)
+    blue = (255, 0, 0)
+    yellow = (0, 255, 255)
+    draw_circle_to_images(img_folder, beats, pos_left_bottom, radius, yellow)
+    if strong_beats is not None:
+        draw_circle_to_images(img_folder, strong_beats, pos_left_bottom, radius, red)
+    draw_circle_to_images(img_folder, start_list, pos_right_top2, radius, black)
+    draw_circle_to_images(img_folder, key_list, pos_right_top3, radius, blue)
+    draw_circle_to_images(img_folder, end_list, pos_right_top4, radius, black)
+
+
+def visualization(area_data, start_list, key_list, end_list, title, beats, group_list, zoom_scale_list):
+    import matplotlib.pyplot as plt
+    x_data = range(len(area_data))
+    # create figure and axis objects with subplots()
+    fig,ax = plt.subplots()
+    # motion data
+    ax.plot(x_data, area_data, color="green",label="motion" , linestyle='-')
+
+    # audio data
+    # vis_y = [area_data[i] for i in beats]
+    # plt.scatter( beats, vis_y, color = "red", label = "audio", marker="x",  s = 10)
+
+    for beat in beats:
+        if beat in group_list:  # group boundaries
+            plt.axvline(x=beat, color='b', linestyle='--')
+        else:
+            plt.axvline(x=beat, color='y', linestyle='--')
+
+    # effect data
+    vis_y = [area_data[i] for i in start_list]
+    plt.scatter(start_list, vis_y, color = "black", label = "start effect",s = 25)
+
+    vis_y = [area_data[i] for i in key_list]
+    plt.scatter(key_list, vis_y, color = "blue", label = "key effect", s = 25)
+
+    # add scale info next to the key point
+    for i, scale in enumerate(zoom_scale_list):
+        ax.annotate(scale, (key_list[i], vis_y[i]))
+
+    vis_y = [area_data[i] for i in end_list]
+    plt.scatter(end_list, vis_y, color = "black", label = "end_effect", s = 25)
+
+    ax.legend( loc="upper left", bbox_to_anchor=(1.05, 1.0))
+    plt.tight_layout()
+    if not os.path.exists("vis_result") :
+        os.mkdir("vis_result")
+    plt.savefig("vis_result/{}.png".format(title))
+
+
+def print_performance():
+    #Audio
+    #Motion
+    #Effect Decision
+    #Video Encoding
+    pass
